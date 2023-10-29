90f2c22259688c3fe14d1b1f01b8de0de36ba205
rellaxa <ewg.nadeev@yandex.ru>
2023-10-29 13:46:04 +0300

    4.1. Аргументы [#295521 #55937]

---
+++ src/main/java/ru/job4j/calculator/ArgMethod.java
@@ -0,0 +1,14 @@
+package ru.job4j.calculator;
+
+public class ArgMethod {
+
+    public static void hello(String name, int age) {
+        System.out.println("Hello, " + name + ", age = " + age);
+    }
+
+    public static void main(String[] args) {
+        String name = "Relaxa";
+        int age = 23;
+        ArgMethod.hello(name, age);
+    }
+}
