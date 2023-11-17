4eb3a55e26c599784395c7ddbb7b8a69ae4d2f41
rellaxa <ewg.nadeev@yandex.ru>
2023-11-17 04:07:32 +0300

    5.2. Создать программу, вычисляющую факториал. [#193 #55991]

--- src/main/java/ru/job4j/loop/LoopFor.java
+++ src/main/java/ru/job4j/loop/LoopFor.java
@@ -2,9 +2,14 @@ package ru.job4j.loop;
 
 public class LoopFor {
     public static void main(String[] args) {
-        for (int i = 5; i <= 10; i++) {
-            System.out.println(i);
-        }
+        int index = 10;
+
+        System.out.println("Использование префиксной формы записи!");
+        System.out.println("Результат вычисления: " + ++index);
+        System.out.println("Значение переменной: " + index);
+        System.out.println();
+        System.out.println("Использование постфиксной формы записи!");
+        System.out.println("Результат вычисления: " + index++);
+        System.out.println("Значение переменной: " + index);
     }
 }
-
