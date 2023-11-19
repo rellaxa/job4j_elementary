ed5fdc5ddc33512efac13127a6ab42620bd8035e
rellaxa <ewg.nadeev@yandex.ru>
2023-11-20 00:09:09 +0300

    6.0.3. Заполнение массива. [#226300 #55901]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,16 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + ages.length);
         System.out.println("Размер массива равен: " + surnames.length);
         System.out.println("Размер массива равен: " + prices.length);
+        System.out.println();
+        String[] names = new String[4];
+        names[0] = "Johnny Depp";
+        names[1] = "Jennifer Lawrence";
+        names[2] = "Jessica Chastain";
+        names[3] = "Ryan Gosling";
+        String actor = names[1];
+        System.out.println(actor);
+        System.out.println(names[3]);
+        System.out.println(names[0]);
+        System.out.println(names[2]);
     }
 }
