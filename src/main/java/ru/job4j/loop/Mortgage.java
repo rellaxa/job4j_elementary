81621d74c9f967de101f369f27f1563db57112ae
rellaxa <ewg.nadeev@yandex.ru>
2023-11-20 16:11:14 +0300

    5.7. Ипотека [#156315 #55980]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -3,11 +3,14 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
-        percent = percent / 100;
         while (amount > 0) {
-            amount += amount * percent - salary;
+            amount += amount * percent / 100 - salary;
             year++;
         }
         return year;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(year(100, 70, 50));
     }
 }
